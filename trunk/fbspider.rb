3b380381cd5200ce0feff2706f30a1fe8286b224
lomato <lomato@eab88b4e-5c3f-0410-814e-2b3882bd23e7>
2010-06-10 13:40:41 +0000

    Added fbspider.

---
+++ trunk/fbspider.rb
@@ -0,0 +1,42 @@
+
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+
+start = 'http://www.facebook.com/barbara.bettetini'
+load_delay = 5
+outfile = 'bettetini.dot'
+
+url_list = Array.new
+url_list << start
+
+file = File.new(outfile, "w")
+
+file.write("graph {\n")
+
+url_list.each do |u|
+    
+    puts "seeking for #{u} (#{url_list.size} pending)"
+
+    doc = Hpricot(open(u, 
+        'User-Agent' => 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.9) ' + 
+        'Gecko/20100330 Fedora/3.5.9-2.fc12 Firefox/3.5.9').read)
+
+    this_user = doc.search("//title").inner_html.split('|')[0].chop
+
+    url_list.delete(u)
+
+    friends = doc.search("//div[@class='UIPortrait_Text']/a")
+    
+    friends.each do |f|
+        #puts "\"#{this_user}\" -- \"#{f[:title]}\""
+        file.write("\"#{this_user}\" -- \"#{f[:title]}\";\n")
+        file.flush
+        url_list << f[:href]
+    end
+
+    sleep load_delay
+end
+
+
+
